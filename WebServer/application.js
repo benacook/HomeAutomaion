50575e32f288da750db87daecfa6e187eefae6c6
benacook <ben.cook@hmkdirect.com>
2018-06-28 13:02:34 +0100

    Added node.js webserver with REST API

--- WebServer/application.js
+++ WebServer/application.js
@@ -1,41 +1,77 @@
-
-
 $(document).ready(function()
 
 {
-  ///////////////////////////////////////////////////////////////////////////////
-  //DEC VAR
-  ///////////////////////////////////////////////////////////////////////////////
-  var
-      Background  = $("svg#Background");
-      LivingOn = $(document).find("#LivingOn");
-      LivingOff = $(document).find("#LivingOff");
-      TempOut = $(document).find("#TempOut");
-  ///////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////
+    //Dec Var
+    ///////////////////////////////////////////////////////////////////////////////
+    var 
+        topRightHumidity = document.getElementById("topRightHumidity");
+        bottomLeftTemp = document.getElementById("bottomLeftTemp");
+        bottomRightButtons = document.getElementById("bottomRightButtons");
+        menuButton = $(document).find("#menu");
+        livingLight = $(document).find("#livingLight");
 
-    setInterval(GetWeather, 10000);
+    ///////////////////////////////////////////////////////////////////////////////
+    //Timed events
+    ///////////////////////////////////////////////////////////////////////////////
+        //setInterval(GetWeather, 10000);
 
-    LivingOn.on('click', function (event)
-    {
+    ///////////////////////////////////////////////////////////////////////////////
+    //Show Menu
+    ///////////////////////////////////////////////////////////////////////////////
+    menuButton.on('click',function (event){
+        if (topRightHumidity.style.display != "none") {
+            topRightHumidity.style.display = "none";
+        }
+        else{
+            topRightHumidity.style.display = "initial";
+        }
 
-      var url = "http://192.168.0.17:8083/ZWaveAPI/Run/devices%5B2%5D.instances%5B0%5D.Basic.Set(255)";
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("POST", url, false);
-      xhttp.send();
+        if (bottomLeftTemp.style.display != "none") {
+            bottomLeftTemp.style.display = "none";
+        }
+        else{
+            bottomLeftTemp.style.display = "initial";
+        }
+
+        if (bottomRightButtons.style.display != "none") {
+            bottomRightButtons.style.display = "none";
+        }
+        else{
+            bottomRightButtons.style.display = "initial";
+        }
     });
 
-    LivingOff.on('click', function (event)
-    {
-
-      var url = "http://192.168.0.17:8083/ZWaveAPI/Run/devices%5B2%5D.instances%5B0%5D.Basic.Set(0)";
-      var xhttp = new XMLHttpRequest();
-      xhttp.open("POST", url, false);
-      xhttp.send();
+    ///////////////////////////////////////////////////////////////////////////////
+    //Toggle Living Room Light
+    ///////////////////////////////////////////////////////////////////////////////
+    livingLight.on('click', function(event){
+        livingLightOn();
     });
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //Living Room Light On
+    ///////////////////////////////////////////////////////////////////////////////
+    function livingLightOn(){
+        var url = "http://raspberrypi:8083/ZWaveAPI/Run/devices%5B2%5D.instances%5B0%5D.Basic.Set(255)";
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", url, false);
+        xhttp.send();
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    //Living Room Light Off
+    ///////////////////////////////////////////////////////////////////////////////
+    function livingLightOff(){
+        var url = "http://raspberrypi:8083/ZWaveAPI/Run/devices%5B2%5D.instances%5B0%5D.Basic.Set(0)";
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", url, false);
+        xhttp.send();
+    }
 
     function GetWeather()
     {
-      var url = "http://api.openweathermap.org/data/2.5/weather?q=Liverpool,uk&units=metric&appid=85fca04eed49d2054facb2000d1ae927";
+      var url = "http://api.openweathermap.org/data/2.5/weather?q=Congleton,uk&units=metric&appid=85fca04eed49d2054facb2000d1ae927";
       var xhttp = new XMLHttpRequest();
       xhttp.open("GET", url, false);
       xhttp.onload = function()
@@ -49,4 +85,5 @@ $(document).ready(function()
       }
       xhttp.send();
     }
+
 });
---
+++ WebServer/myapi.js
@@ -0,0 +1,88 @@
+/**
+ * myapi.js
+ * 
+ * 
+ * @version 1.0.0 - initial
+ *
+ * 
+ * DESCRIPTION:
+ * 
+ * 
+ * @throws none
+ * @see nodejs.org
+ * @see express.org
+ * 
+ * @author Ben Cook
+ */
+
+var http      = require('http');
+var express   = require('express');
+
+var app       = express();
+
+var climate = [ {humidity: '0', temperature: '0'},
+                {humidity: '0', temperature: '0'},
+                {humidity: '0', temperature: '0'},
+                {humidity: '0', temperature: '0'}
+              ];
+
+//=============================================================================
+// configure Express to serve index.html and any other static pages stored 
+// in the home directory
+//=============================================================================
+app.use(express.static(__dirname));
+
+//=============================================================================
+// Receives Climate data from a client
+//=============================================================================
+app.get('/api/climate/set/:id/humidity=:hum&temperature=:temp', function (req, res) {
+  console.log('id = ' + req.params.id);
+  //save data
+  climate[req.params.id].humidity = req.params.hum;
+  climate[req.params.id].temperature = req.params.temp;
+  //echo back to client
+  res.send(climate[req.params.id]);
+}); // apt.get()
+
+//=============================================================================
+// Sends Climate data by id to a client
+//=============================================================================
+app.get('/api/climate/get/:id', function (req, res) {
+  console.log('id = ' + req.params.id);
+  //send data for id
+  res.send(climate[req.params.id]);
+}); // apt.get()
+
+//=============================================================================
+// Sends all Climate data to a client
+//=============================================================================
+app.get('/api/climate/get', function (req, res) {
+  console.log('id = ' + req.params.id);
+  //send data for id
+  res.send(climate);
+}); // apt.get()
+
+//=============================================================================
+// Express route for any other unrecognised incoming requests
+//=============================================================================
+app.get('*', function (req, res) {
+  res.status(404).send('Unrecognised API call');
+});
+
+//=============================================================================
+// Express route to handle errors
+//=============================================================================
+app.use(function (err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send('Oops, Something went wrong!');
+  } else {
+    next(err);
+  }
+}); // apt.use()
+
+//=============================================================================
+// Start Express App Server
+//=============================================================================
+app.listen(3000);
+console.log('App Server is listening on port 3000');
+
