790801f312072abe9c328847b58a114242aa7511
benacook <37549856+benacook@users.noreply.github.com>
2018-10-17 21:49:21 +0100

    Added MySQL

--- WebServer/myapi.js
+++ WebServer/myapi.js
@@ -1,22 +1,18 @@
 /**
- * myapi.js
- * 
- * 
+ * homehubapi.js
+ *  
  * @version 1.0.0 - initial
  *
- * 
  * DESCRIPTION:
- * 
- * 
- * @throws none
- * @see nodejs.org
- * @see express.org
- * 
+ * node.js server for Raspberry Pi to collect data from sensors and serve 
+ * a web based user interface to allow control of light switches and
+ * reading data 
  * @author Ben Cook
  */
 
 var http      = require('http');
 var express   = require('express');
+var mysql = require('mysql'); 
 
 var app       = express();
 
@@ -25,6 +21,35 @@ var climate = [ {humidity: '0', temperature: '0'},
                 {humidity: '0', temperature: '0'},
                 {humidity: '0', temperature: '0'}
               ];
+
+//=============================================================================
+// COnnect to MySQL Database
+//=============================================================================
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "",
+  password: "",
+  database: ""
+});
+//MySQL Connect
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to MySQL database");})
+
+//=============================================================================
+// js date format to MySQL format
+//=============================================================================
+function twoDigits(d) {
+  if(0 <= d && d < 10) return "0" + d.toString();
+  if(-10 < d && d < 0) return "-0" + (-1*d).toString();
+  return d.toString();
+}
+
+Date.prototype.toMysqlFormat = function() {
+  return this.getUTCFullYear() + "-" + twoDigits(1 + this.getUTCMonth()) +
+   "-" + twoDigits(this.getUTCDate()) + " " + twoDigits(this.getUTCHours()) + 
+   ":" + twoDigits(this.getUTCMinutes()) + ":" + twoDigits(this.getUTCSeconds());
+};
 
 //=============================================================================
 // configure Express to serve index.html and any other static pages stored 
@@ -36,12 +61,36 @@ app.use(express.static(__dirname));
 // Receives Climate data from a client
 //=============================================================================
 app.get('/api/climate/set/:id/humidity=:hum&temperature=:temp', function (req, res) {
-  console.log('id = ' + req.params.id);
+  console.log('id: ' + req.params.id + ' hum:' + req.params.hum +
+  ' temp:' + req.params.temp);
+
   //save data
   climate[req.params.id].humidity = req.params.hum;
   climate[req.params.id].temperature = req.params.temp;
+
   //echo back to client
   res.send(climate[req.params.id]);
+
+  //get date time
+  var dt = new Date().toMysqlFormat();
+  dt = "'"+dt+"'";
+  var HumQuery = "INSERT INTO Humidity (DateTime, Humidity, ID) VALUES ("+ 
+    dt + ","+ req.params.hum + "," + req.params.id+")";
+  var TempQuery = "INSERT INTO Temperature (DateTime, Temperature, ID) VALUES ("+
+    dt + ","+ req.params.temp + "," + req.params.id+")";
+
+  //MySQL Log Humidity
+  con.query(HumQuery, function (err, result) {
+    if (err) throw err;
+    console.log("Humidity Logged to MySQL");
+  });
+
+  //MySQL Log Temperature
+  con.query(TempQuery, function (err, result) {
+    if (err) throw err;
+    console.log("Temperature Logged to MySQL");
+  });
+
 }); // apt.get()
 
 //=============================================================================
@@ -62,18 +111,18 @@ app.get('/api/climate/get', function (req, res) {
 }); // apt.get()
 
 //=============================================================================
-// Express route for any other unrecognised incoming requests
+// Unrecognised requests
 //=============================================================================
 app.get('*', function (req, res) {
   res.status(404).send('Unrecognised API call');
 });
 
 //=============================================================================
-// Express route to handle errors
+// Error Hadling
 //=============================================================================
 app.use(function (err, req, res, next) {
   if (req.xhr) {
-    res.status(500).send('Oops, Something went wrong!');
+    res.status(500).send('Error!');
   } else {
     next(err);
   }
